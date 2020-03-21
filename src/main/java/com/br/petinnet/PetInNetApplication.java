3707252ccc53654146d9b30756723f7016870d4b
Lucas <lucasrdeni@hotmail.com>
2020-03-21 14:34:58 -0300

    Login and Registration working

---
+++ src/main/java/com/br/petinnet/PetInNetApplication.java
@@ -0,0 +1,12 @@
+package com.br.petinnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetInNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetInNetApplication.class, args);
+    }
+}
---
+++ src/main/java/com/br/petinnet/service/UserService.java
@@ -0,0 +1,46 @@
+package com.br.petinnet.service;
+
+import com.br.petinnet.model.User;
+import com.br.petinnet.repository.RoleRepository;
+import com.br.petinnet.model.Role;
+import com.br.petinnet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User findUserByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+}
